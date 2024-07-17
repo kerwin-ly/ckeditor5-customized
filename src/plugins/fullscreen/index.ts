3965c6240e435922f7b1573382f8658ecc1bde3f
eeemmafeng <1752475434@qq.com>
2024-07-17 10:45:19 +0800

    fix: restore the height of editorContainer

--- src/plugins/fullscreen/index.ts
+++ src/plugins/fullscreen/index.ts
@@ -31,9 +31,15 @@ class Fullscreen extends Plugin {
 
       const editorContainer = editor.ui.view.editable.element?.parentElement!;
       const editorParentNode = editorContainer?.parentElement!;
+      const originHeight = editorContainer.style.height;
 
       view.on("execute", () => {
-        this.toggleFullscreen(editor, editorContainer, editorParentNode);
+        this.toggleFullscreen(
+          editor,
+          editorContainer,
+          editorParentNode,
+          originHeight
+        );
       });
 
       return view;
@@ -43,14 +49,20 @@ class Fullscreen extends Plugin {
   private toggleFullscreen(
     editor: Editor,
     editorContainer: HTMLElement,
-    editorParentNode: HTMLElement
+    editorParentNode: HTMLElement,
+    originHeight: string
   ) {
     this.isFullscreen = !this.isFullscreen;
 
     if (this.isFullscreen) {
       this.enterFullscreen(editor, editorContainer, editorParentNode);
     } else {
-      this.exitFullscreen(editor, editorContainer, editorParentNode);
+      this.exitFullscreen(
+        editor,
+        editorContainer,
+        editorParentNode,
+        originHeight
+      );
     }
     if (typeof this.fullscreenCb === "function") {
       this.fullscreenCb(this.isFullscreen);
@@ -79,7 +91,7 @@ class Fullscreen extends Plugin {
     newElement.style.position = "fixed";
     newElement.style.width = "100%";
     newElement.style.height = "100%";
-    newElement.style.zIndex = "9999";
+    newElement.style.zIndex = "9998";
 
     element.style.position = "relative";
     element.style.width = "100%";
@@ -93,17 +105,24 @@ class Fullscreen extends Plugin {
   private exitFullscreen(
     editor: Editor,
     element: HTMLElement,
-    parentNode: HTMLElement
+    parentNode: HTMLElement,
+    originHeight: string
   ) {
     document.body.removeChild(document.querySelector(".editor-fullscreen")!);
     parentNode.appendChild(element);
+
+    if (originHeight) {
+      element.style.height = originHeight;
+    } else {
+      element.style.height = "";
+    }
 
     element.style.marginTop = "50px";
     element.style.position = "";
     element.style.top = "";
     element.style.left = "";
     element.style.width = "";
-    element.style.height = "";
+    // element.style.height = "";
     element.style.zIndex = "";
     element.style.backgroundColor = "";
     // this.isFullscreen = false;
